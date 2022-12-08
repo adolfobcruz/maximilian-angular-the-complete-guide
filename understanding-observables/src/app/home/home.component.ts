45430e97b5c0f185abd2f7b8ea1a9f500d024b66
Adolfo Cruz <adolfo@rewrite.com.br>
2022-12-08 17:18:17 -0400

    main - class #176

--- understanding-observables/src/app/home/home.component.ts
+++ understanding-observables/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -29,14 +30,18 @@ export class HomeComponent implements OnInit, OnDestroy {
         }
 
         if (count > 3) {
-          observer.error(new Error('Count is greater than 3!'));
+          observer.error(new Error('Round is greater than 3!'));
         }
 
         count++;
       }, 1000);
     });
 
-    this.firstObserverSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObserverSubscription = customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
