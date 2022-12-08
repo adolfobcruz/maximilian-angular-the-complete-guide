c813ff1b52a7d3a2c2b33dff8461e2f3b93a3daa
Adolfo Cruz <adolfo@rewrite.com.br>
2022-12-08 14:53:35 -0400

    main - class #173

--- understanding-observables/src/app/home/home.component.ts
+++ understanding-observables/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +14,22 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstObserverSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    })
+    // this.firstObserverSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObserverSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
   }
 
   ngOnDestroy(): void {
