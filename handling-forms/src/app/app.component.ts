d1b867edec7555e7abf2fcad863e6925d0665c47
Adolfo Cruz <adolfo@rewrite.com.br>
2022-12-12 14:33:40 -0400

    main - class #203

--- handling-forms/src/app/app.component.ts
+++ handling-forms/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  signUpForm = FormGroup;
+export class AppComponent implements OnInit {
+  signUpForm  = FormGroup;
   genders = ['male', 'female'];
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    });
+  }
 }
