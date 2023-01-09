65cb52718fa8ada9fab6693dbc27957f6f2c2493
Adolfo Cruz <adolfo@rewrite.com.br>
2023-01-09 17:18:06 -0400

    main - class #33

--- 02-the-basics/my-first-app/src/app/app.module.ts
+++ 02-the-basics/my-first-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,8 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
