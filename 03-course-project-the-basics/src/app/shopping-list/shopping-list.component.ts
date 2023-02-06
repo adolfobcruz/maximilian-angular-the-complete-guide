432a8803b8eb019a6bf62bb7c1412e2ee620ea37
Adolfo Cruz <adolfo@rewrite.com.br>
2023-02-06 16:36:46 -0400

    main - class #57

--- 03-course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
+++ 03-course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredients.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [];
 
   constructor() { }
 
