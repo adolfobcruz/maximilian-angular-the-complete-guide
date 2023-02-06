56907711f73c20848709f680c90f4279b6647506
Adolfo Cruz <adolfo@rewrite.com.br>
2023-02-06 11:48:04 -0400

    main - class #54

--- 03-course-project-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 03-course-project-the-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Arroz com carne moída', 'O melhor prato de todos', 'https://i0.hippopx.com/photos/581/867/771/minced-meat-minced-meat-meat-schweinemett-preview.jpg')
+    new Recipe('Arroz com carne moída', 'O melhor prato de todos', 'https://i0.hippopx.com/photos/581/867/771/minced-meat-minced-meat-meat-schweinemett-preview.jpg'),
+    new Recipe('Macarrão Alho e Óleo', 'O segundo melhor prato de todos', 'https://i0.hippopx.com/photos/581/867/771/minced-meat-minced-meat-meat-schweinemett-preview.jpg'),
   ];
 
   constructor() { }
