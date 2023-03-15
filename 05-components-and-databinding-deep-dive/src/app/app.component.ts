69c8a2d4245fea49a560bf81512ae9edbe32346c
Adolfo Cruz <adolfo@rewrite.com.br>
2023-03-15 08:15:04 -0400

    main - class #68

--- 05-components-and-databinding-deep-dive/src/app/app.component.ts
+++ 05-components-and-databinding-deep-dive/src/app/app.component.ts
@@ -9,13 +9,24 @@ export class AppComponent {
   serverElements = [
     {
       type: 'server',
-      name: 'Testserver1',
-      content: 'Just a test1!'
-    },
-    {
-      type: 'server',
-      name: 'Testserver2',
-      content: 'Just a test2!'
+      name: 'Testserver',
+      content: 'Just a test!'
     },
   ];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
