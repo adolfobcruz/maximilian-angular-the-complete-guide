309e989ee71b826f57fc362d2e43d50f84b4f5b4
Adolfo <adolfo@rewrite.com.br>
2024-01-17 20:12:08 -0400

    main - class #260

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -11,10 +11,11 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
     this.http
       .post(
         'https://lab-angular-maximilian-default-rtdb.firebaseio.com/posts.json',
@@ -26,10 +27,18 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
-  onClearPosts() {
-    // Send Http request
+  onClearPosts() {}
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://lab-angular-maximilian-default-rtdb.firebaseio.com/posts.json'
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
