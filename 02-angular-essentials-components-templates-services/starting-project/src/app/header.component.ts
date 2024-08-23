e674a616b832c6b43b8eb5eba80904f505fad187
rwtrecs <adolfo@rewrite.com.br>
2024-08-23 14:59:31 -0400

    main - class #14

---
+++ 02-angular-essentials-components-templates-services/starting-project/src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './app.component.html',
+
+})
+export class HeaderComponent  {}
