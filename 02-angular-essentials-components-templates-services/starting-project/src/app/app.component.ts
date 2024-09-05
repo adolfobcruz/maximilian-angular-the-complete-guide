ad32bedfef3aaa017239a00109e1cd1264c88ba2
rwtrecs <adolfo@rewrite.com.br>
2024-09-05 14:27:02 -0400

    main - class #37

--- 02-angular-essentials-components-templates-services/starting-project/src/app/app.component.ts
+++ 02-angular-essentials-components-templates-services/starting-project/src/app/app.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  @Output() select = new EventEmitter<string>();
+
   users = DUMMY_USERS;
+  selectUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('Select user with ID ' + id);
+    this.selectUserId = id;
   }
 }
