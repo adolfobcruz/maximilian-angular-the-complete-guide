4a043fc4661a96d2006a687dcead018d49c572d8
rwtrecs <adolfo@rewrite.com.br>
2024-09-24 14:58:03 -0400

    main - class #65

---
+++ 03-angular-essentials-working-with-modules/starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { type Task } from './task.model';
+
+import { TasksService } from '../tasks.service';
+
+import { CardComponent } from '../../shared/card/card.component';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CardComponent, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
+  }
+}
